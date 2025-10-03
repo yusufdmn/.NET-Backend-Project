5d3ddbb1ca96b730262d04d7cda380f500606a0a
yusufduman <yusufveduman@gmail.com>
2025-10-03 22:27:59 +0300

    Add Autofac to the project for IoC.

--- ECommerce/WebAPI/Program.cs
+++ ECommerce/WebAPI/Program.cs
@@ -1,5 +1,8 @@
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
 using Business.Abstract;
 using Business.Concrete;
+using Business.DependencyResolvers.Autofac;
 using DataAccess.Abstract;
 using DataAccess.Concrete.EntityFramework;
 
@@ -9,14 +12,16 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
-// Dependency Injection
-builder.Services.AddScoped<IProductService, ProductService>();
-builder.Services.AddScoped<IProductDal, EFProductDal>();
+// add autofac
+builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory());
+builder.Host.ConfigureContainer<Autofac.ContainerBuilder>(containerBuilder =>
+{
+    containerBuilder.RegisterModule(new AutofacBusinessModule());
+});
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
 
 
 
